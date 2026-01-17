e834ba057fa180d096794576b856c3f53581cb21
krismile000 <616901156@qq.com>
2026-01-17 14:00:33 +0800

    genxin

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
 import { z } from 'zod'
 import { getDb } from '../db.js'
 import { signToken } from '../utils/jwt.js'
